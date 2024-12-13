8e899857eff5b626f4151175388e677c23a129c4
jzeron <jon@code.city>
2024-12-12 17:28:54 -0800

    tailwind

--- react-typescript-tailwind-vite/src/App.tsx
+++ react-typescript-tailwind-vite/src/App.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function App() {
   return (
-    <div>
-      <h1>Welcome to your new project!</h1>
+    <div className='flex items-center justify-center w-screen h-screen bg-gradient-to-b from-blue-950 via-indigo-500 to-purple-950'>
+      <h1 className='text-white text-4xl font-thin'>Welcome to your new app!</h1>
     </div>
   )
 }
